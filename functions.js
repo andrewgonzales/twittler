22c5114cb6f382b5f15a54b3da353974cc15dff4
Andrew Gonzales <andrewmartingonzales@gmail.com>
2015-07-18 23:24:16 -0600

    Removed underscores

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-_.each = function(collection, iterator) {
+each = function(collection, iterator) {
     if(Array.isArray(collection)){
       for(var i = 0; i<collection.length; i++){
         iterator(collection[i], i, collection);
@@ -10,9 +10,9 @@ _.each = function(collection, iterator) {
     }
   };
 
- _.filter = function(collection, test) {
+ filter = function(collection, test) {
     var passed = [];
-    _.each(collection, function(item){
+    each(collection, function(item){
       if(test(item)){
         passed.push(item);
       }
@@ -20,22 +20,25 @@ _.each = function(collection, iterator) {
     return passed;
   };
 
-_.map = function(collection, iterator) {
+map = function(collection, iterator) {
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
     var mapped = [];
-    _.each(collection, function(item){
+    each(collection, function(item){
       mapped.push(iterator(item));
     });
     return mapped;
   };
 
-_.pluck = function(collection, key) {
+pluck = function(collection, key) {
     // TIP: map is really handy when you want to transform an array of
     // values into a new array of values. _.pluck() is solved for you
     // as an example of this.
-    return _.map(collection, function(item){
+    return map(collection, function(item){
       return item[key];
     });
   };
+  var getUserTweets = function(username){
+    return tweetData = filter(streams.home, function(tweet){return tweet.user===username;});
+  }
